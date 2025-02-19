c90871eaeaae4a86b7a745777565d05c7719b1d4
pedroivo1 <pedroivoal1@gmail.com>
2025-02-19 16:51:33 -0300

    tutorial 8

--- Tutorials/1. hello world.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication, QLabel
-
-app = QApplication(sys.argv)
-label = QLabel("Hello World!")
-label = QLabel("<font color=red size=40>Hello World!</font>")
-label.show()
-app.exec()
---
+++ Tutorials/8. qrc files/qrc files.py
@@ -0,0 +1,4 @@
+from PySide6.QtGui import QIcon, QKeySequence, QPixmap
+import rc_icons
+
+playIcon = QIcon(QPixmap(':/icons/play.png'))
